bb459680a8c159077c335ba8ab9851fb6213306c
Denis Khaidarshin <denis.khaidarshin@arcadia.spb.ru>
2018-11-04 18:20:59 +0300

    added mongo mongoose

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,19 +1,34 @@
 const graphql = require('graphql');
+const mongoose = require('mongoose');
+const {connection, Schema} = mongoose;
 
+mongoose.connect('mongodb://localhost:27017/my-super-db',
+    {useNewUrlParser: true});
 
+connection.on('error', (e) => {
+  console.error('Connection error: ', e);
+});
+connection.once('open', () => {
+  console.log('Connection established');
+});
+const bookSchema = new Schema({
+  name: String,
+  genre: String,
+});
 
+const Book = mongoose.model('book', bookSchema);
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLList} = graphql;
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => {
     return {
-      id: { type: GraphQLString },
-      name: { type: GraphQLString },
-      genre: { type: GraphQLString }
-    }
-  }
+      id: {type: GraphQLString},
+      name: {type: GraphQLString},
+      genre: {type: GraphQLString},
+    };
+  },
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -21,14 +36,20 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: {id: { type: GraphQLString }},
-      resolve(resolve, args) {
-
-      }
-    }
-  }
+      args: {id: {type: GraphQLString}},
+      resolve: async (parent, args) => {
+        return await Book.findById(args.id);
+      },
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: async (parent, args) => {
+        return Book.find({});
+      },
+    },
+  },
 });
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
 });
