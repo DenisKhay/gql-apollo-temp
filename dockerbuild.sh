fe2fea94fd053f1810ddefc2f43bf3c28b213d68
denis.khaydarshin <dragolietel@gmail.com>
2018-11-14 01:04:07 +0300

    wip

--- dockerbuild.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker stop webapi-precious-eva
-docker image rm webapi-eva:latest
-docker build --tag webapi-eva:latest . > dockerbuild.log
--- dockerrun.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-docker run -p 27017:27017 -v $(pwd)/dbdata:/data/db \
--e MONGO_INITDB_ROOT_USERNAME=usernme \
--e MONGO_INITDB_ROOT_PASSWORD=simple \
---name mongo-eva \
---restart always mongo:latest
